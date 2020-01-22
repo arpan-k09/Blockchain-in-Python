dbdc075f5aa2f5092ad1d842956dea702e474f60
arpan-k09 <48683269+arpan-k09@users.noreply.github.com>
2020-01-22 07:00:18 +0530

    Add files via upload

---
+++ b1/utility/__init__.py
@@ -0,0 +1,5 @@
+from utility.hash_util import hash_string_256
+
+'''Not Export anything except __all__ var. we can also put single _ before methods or vars to stop exporting'''
+
+__all__ = ['hash_string_256']
---
+++ b1/utility/hash_util.py
@@ -0,0 +1,25 @@
+import hashlib as hl
+import json
+
+
+'''Not Export anything except __all__ var. we can also put single _ before methods or vars to stop exporting'''
+# __all__ = ['hash_string_256','hash_block','hl']
+
+def hash_string_256(string):
+    """Create a SHA256 hash for a given input string.
+
+    Arguments:
+        :string: The string which should be hashed.
+    """
+    return hl.sha256(string).hexdigest()
+
+
+def hash_block(block):
+    """Hashes a block and returns a string representation of it.
+
+    Arguments:
+        :block: The block that should be hashed.
+    """
+    hashable_block = block.__dict__.copy()
+    hashable_block['transactions'] = [tx.to_ordered_dict() for tx in hashable_block['transactions']]
+    return hash_string_256(json.dumps(hashable_block, sort_keys=True).encode())
