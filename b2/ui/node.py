0ff76777efd72ac6c94863fd033bc1cfc5326671
arpan-k09 <48683269+arpan-k09@users.noreply.github.com>
2020-01-22 07:01:25 +0530

    Add files via upload

---
+++ b2/ui/node.py
@@ -0,0 +1,201 @@
+from flask import Flask, jsonify, request, send_from_directory
+'''jasonify takes data and automatically converts into the json data.........'''
+from flask_cors import CORS
+
+from wallet import Wallet
+from blockchain import Blockchain
+
+
+app = Flask(__name__)
+wallet = Wallet()
+'''We're passing None value of public key to the blockchain constructor...'''
+blockchain = Blockchain(wallet.public_key)
+CORS(app)
+
+
+'''It's for response from server so we're sending back a res through a GET method..
+and route is a end point and app is our application name above we can change it to anything we
+want'''
+@app.route('/', methods=['GET'])
+def get_ui():
+    #return 'This works!!!!'
+    '''For the redirecting towards the html file uses send_from_directory function '''
+    return send_from_directory('ui', 'node.html')
+
+# '''Starting phase is this............'''
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
+
+
+
+
+@app.route('/wallet', methods=['POST'])
+def create_keys():
+    wallet.create_keys()
+    # wallet.save_keys()
+    # response = {
+    #     'public_key': wallet.public_key,
+    #     'private_key': wallet.private_key,
+    #     'funds': blockchain.get_balance()
+    # }
+    if wallet.save_keys():
+        global blockchain
+        blockchain = Blockchain(wallet.public_key)
+        response = {
+            'public_key': wallet.public_key,
+            'private_key': wallet.private_key,
+            'funds': blockchain.get_balance()
+        }
+        #blockchain = Blockchain(wallet.public_key)
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'Saving the keys failed.'
+        }
+        return jsonify(response), 500
+
+
+@app.route('/wallet', methods=['GET'])
+def load_keys():
+    if wallet.load_keys():
+        global blockchain
+        blockchain = Blockchain(wallet.public_key)
+        response = {
+            'public_key': wallet.public_key,
+            'private_key': wallet.private_key,
+            'funds': blockchain.get_balance()
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'Loading the keys failed.'
+        }
+        return jsonify(response), 500
+
+
+@app.route('/balance', methods=['GET'])
+def get_balance():
+    balance = blockchain.get_balance()
+    if balance != None:
+        response = {
+            'message': 'Fetched balance successfully.',
+            'funds': balance
+        }
+        return jsonify(response), 200
+    else:
+        response = {
+            'messsage': 'Loading balance failed.',
+            'wallet_set_up': wallet.public_key != None
+        }
+        return jsonify(response), 500
+
+
+@app.route('/transaction', methods=['POST'])
+def add_transaction():
+    if wallet.public_key == None:
+        response = {
+            'message': 'No wallet set up.'
+        }
+        return jsonify(response), 400
+    values = request.get_json()
+    if not values:
+        response = {
+            'message': 'No data found.'
+        }
+        return jsonify(response), 400
+    required_fields = ['recipient', 'amount']
+    if not all(field in values for field in required_fields):
+        response = {
+            'message': 'Required data is missing.'
+        }
+        return jsonify(response), 400
+
+    recipient = values['recipient']
+    amount = values['amount']
+
+    signature = wallet.sign_transaction(wallet.public_key, recipient, amount)
+    success = blockchain.add_transaction(recipient, wallet.public_key, signature, amount)
+
+    if success:
+        response = {
+            'message': 'Successfully added transaction.',
+            'transaction': {
+                'sender': wallet.public_key,
+                'recipient': recipient,
+                'amount': amount,
+                'signature': signature
+            },
+            'funds': blockchain.get_balance()
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'Creating a transaction failed.'
+        }
+        return jsonify(response), 500
+
+
+@app.route('/mine', methods=['POST'])
+def mine():
+    '''using mine_block method which is inside a blockchain file.......'''
+    block = blockchain.mine_block()
+    # dict_block = block.__dict__.copy()
+    # dict_block['transactions'] = [
+    #     tx.__dict__ for tx in dict_block['transactions']]
+
+
+    # if blockchain.mine_block():
+    #     return ...
+    # else:
+    #     response = {
+    #         'message': 'Adding a block failed.',
+    #         'wallet_set_up': wallet.public_key != None
+    #     }
+    #     return jsonify(response), 500
+
+
+    if block != None:
+        dict_block = block.__dict__.copy()
+        dict_block['transactions'] = [
+            tx.__dict__ for tx in dict_block['transactions']]
+        response = {
+            'message': 'Block added successfully.',
+            'block': dict_block,
+            'funds': blockchain.get_balance()
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'Adding a block failed.',
+            'wallet_set_up': wallet.public_key != None
+        }
+        return jsonify(response), 500
+
+
+@app.route('/transactions', methods=['GET'])
+def get_open_transaction():
+    transactions = blockchain.get_open_transactions()
+    dict_transactions = [tx.__dict__ for tx in transactions]
+
+    # response = {
+    #     'message' : 'Fetched transaction successfully..',
+    #     'transactions' : dict_transactions
+    # }
+    return jsonify(dict_transactions), 200
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_snapshot = blockchain.chain
+    '''we need to convert the objects to json serializable so we're using list comp.....'''
+    di_chain = [block.__dict__.copy() for block in chain_snapshot]
+    for d_b in di_chain:
+        d_b['transactions'] = [tx.__dict__ for tx in d_b['transactions']]
+
+    #     '''second in return statment is a status code and 200 is for OK'''
+    #     return jsonify(chain_snapshot), 200
+    return jsonify(di_chain), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ b2/wallet.py
@@ -0,0 +1,94 @@
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
+import Crypto.Random
+
+'''binascii converts binary to ascii value'''
+import binascii
+
+class Wallet:
+    """Creates, loads and holds private and public keys. Manages transaction signing and verification."""
+
+    def __init__(self):
+        # private_key, public_key = self.generate_keys()
+        # self.private_key = private_key
+        # self.public_key = public_key
+        self.private_key = None
+        self.public_key = None
+
+    def create_keys(self):
+        """Create a new pair of private and public keys."""
+        private_key, public_key = self.generate_keys()
+        self.private_key = private_key
+        self.public_key = public_key
+        # with open('wallet.txt', mode='w') as f:
+        #     f.write(self.public_key)
+        #     f.write('\n')
+        #     f.write(self.private_key)
+
+    def save_keys(self):
+        """Saves the keys to a file (wallet.txt)."""
+        if self.public_key != None and self.private_key != None:
+            try:
+                with open('wallet.txt', mode='w') as f:
+                    f.write(self.public_key)
+                    f.write('\n')
+                    f.write(self.private_key)
+                return True
+            except (IOError, IndexError):
+                print('Saving wallet failed...')
+                return False
+
+    def load_keys(self):
+        """Loads the keys from the wallet.txt file into memory."""
+        try:
+            with open('wallet.txt', mode='r') as f:
+                keys = f.readlines()
+                '''At above we can see new line is add esacap char so we don't need it so we're using [:-1] range selector '''
+                public_key = keys[0][:-1]
+                private_key = keys[1]
+                self.public_key = public_key
+                self.private_key = private_key
+            return True
+        except (IOError, IndexError):
+            print('Loading wallet failed...')
+            return False
+
+    def generate_keys(self):
+        """Generate a new pair of private and public key."""
+        '''First arg of the RSA.generate is a bit length and second is random function!!!'''
+        private_key = RSA.generate(1024, Crypto.Random.new().read)
+        '''Public key is generated from the Private key so both are related to each other!!!!'''
+        public_key = private_key.publickey()
+        '''hexlify is for hexadecimal repre.... of binary data ... and DER is binary encoding'''
+        return (binascii.hexlify(private_key.exportKey(format='DER')).decode('ascii')
+                , binascii.hexlify(public_key.exportKey(format='DER')).decode('ascii'))
+
+    def sign_transaction(self, sender, recipient, amount):
+        """Sign a transaction and return the signature.
+
+        Arguments:
+            :sender: The sender of the transaction.
+            :recipient: The recipient of the transaction.
+            :amount: The amount of the transaction.
+        """
+        signer = PKCS1_v1_5.new(RSA.importKey(binascii.unhexlify(self.private_key)))
+        h = SHA256.new((str(sender) + str(recipient) + str(amount)).encode('utf8'))
+        signature = signer.sign(h)
+        return binascii.hexlify(signature).decode('ascii')
+
+    @staticmethod
+    def verify_transaction(transaction):
+        """Verify the signature of a transaction.
+
+        Arguments:
+            :transaction: The transaction that should be verified.
+        """
+
+        # if transaction.sender == 'MINING':
+        #     return True
+
+        public_key = RSA.importKey(binascii.unhexlify(transaction.sender))
+        verifier = PKCS1_v1_5.new(public_key)
+        h = SHA256.new((str(transaction.sender) + str(transaction.recipient) + str(transaction.amount)).encode('utf8'))
+        return verifier.verify(h, binascii.unhexlify(transaction.signature))
